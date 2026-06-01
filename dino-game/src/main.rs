1e009ca175b7b918de9d79592f076f14641a9a68
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2026-06-01 12:57:57 -0700

    blank window for a sec

--- dino-game/src/main.rs
+++ dino-game/src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Dino Quiz!".into(),
+                resolution: (800., 600.).into(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
